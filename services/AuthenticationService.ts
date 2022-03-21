4cfd3b3eced439fefc9d2cc852548a93a0a5bb2b
Parth Shah <parth.shah2118@gmail.com>
2022-03-21 18:50:24 -0400

    first commit

--- services/AuthenticationService.ts
+++ services/AuthenticationService.ts
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 
 const userDao: UserDao = UserDao.getInstance();
 
-const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = "giuseppi";//process.env.DB_USERNAME;
-const DB_PASSWORD = "supersecretpassword";//process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
-const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+// const PROTOCOL = "mongodb+srv";
+// const DB_USERNAME = "giuseppi";//process.env.DB_USERNAME;
+// const DB_PASSWORD = "supersecretpassword";//process.env.DB_PASSWORD;
+// const HOST = "cluster0.m8jeh.mongodb.net";
+// const DB_NAME = "myFirstDatabase";
+// const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = "mongodb+srv://parth2118:Parth2608@cluster0.e4v81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // connect to the database
 mongoose.connect(connectionString);
 
