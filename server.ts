1a9b2cdd9b390112dbeeac52a9057ee16bf12fdc
Parth Shah <parth.shah2118@gmail.com>
2022-03-29 17:49:28 -0400

    first commit

--- server.ts
+++ server.ts
@@ -29,8 +29,8 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const HOST = "cluster0.e4v81.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
-// const connectionString = "mongodb+srv://parth2118:Parth2608@cluster0.e4v81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";// connect to the database
+//const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
+const connectionString = "mongodb+srv://parth2118:Parth2608@cluster0.e4v81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";// connect to the database
 mongoose.connect(connectionString);
 
 const app = express();
